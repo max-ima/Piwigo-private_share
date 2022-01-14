f3316d6eb94dd716476d2b05abc9783c22a7e03b
max-ima <96241406+max-ima@users.noreply.github.com>
2022-01-14 07:10:16 +0100

    sl_SI
    Slovenian language

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,18 @@
+<?php
+$lang['Invalid email address'] = 'Nepravilen e-mail naslov';
+$lang['Close'] = 'Zapri';
+$lang['Share'] = 'Delite';
+$lang['Expires in'] = 'Poteče čez';
+$lang['Send email'] = 'Pošlji e-pošto';
+
+
+$lang['Apply to albums'] = 'Uporabi za albume';
+$lang['all albums'] = 'vsi albumi';
+$lang['a list of albums'] = 'seznam albumov';
+
+$lang['Activity'] = 'Aktivnost';
+$lang['Expiration'] = 'Poteče';
+$lang['Recipient'] = 'Prejemnik';
+$lang['Photo'] = 'Fotografija';
+$lang['Search:'] = 'Išči:';
+?>
